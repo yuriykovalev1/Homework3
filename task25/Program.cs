79e15361f0f14cb0cc3d2d3e6a804cf8c46a8a6b
Yuriy Kovalev <yuriykovalev1984@gmail.com>
2023-10-22 21:58:26 +0300

    Семинар 3 задачи

---
+++ task25/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B
+
+Console.WriteLine("введите число A");           // вводим первое число
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите степень числа А");   // вводим степень числа
+int b = Convert.ToInt32(Console.ReadLine());
+int stepen = a;
+if (b < 1)
+    Console.Write("Степень должна быть натуральным\n");     // если степень 0
+else
+{
+    for (int i = 1; i < b; i++)                             // возводим в степень
+    {
+        stepen = stepen * a;
+        Console.WriteLine("A в степени B равно: " + stepen);
+    }
+}
---
+++ task27/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int number = ReadInt("Введите число: ");
+int len = NumberLen(number);
+SumNumbers(number, len);
+
+int ReadInt(string message) // Функция ввода
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int NumberLen(int a) // Функция подсчета цифр в числе
+{
+    int index = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        index++;
+    }
+    return index;
+}
+void SumNumbers(int n, int len) // Функция вывода суммы цифр в числе
+{
+    int sum = 0;
+    for (int i = 1; i <= len; i++)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    Console.Write($"Сумма цифр в числе {number} равно: {sum}");
+}
